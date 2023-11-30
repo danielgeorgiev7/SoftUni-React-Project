ad72ce7fab5f82c636e2ea703ae9105c348a555e
nymuelis <daniel.tsv.georgiev@gmail.com>
2023-11-30 04:35:33 +0200

    Added login page styling, animations

--- Vista-Madridista/src/main.jsx
+++ Vista-Madridista/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./main.css";
+import "./animations.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
---
+++ Vista-Madridista/src/utils/isValidEmail.js
@@ -0,0 +1,6 @@
+export default function isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+
+    return !!emailRegex.test(email);
+}
+
